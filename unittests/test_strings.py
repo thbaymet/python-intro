72839dea30e7c6564b329085edef48207ad4d622
BAYRAMOV Matin <bayramov.matin@gmail.com>
2018-11-07 07:05:07 +0100

    Added unittests example.

--- unittests/test_strings.py
+++ unittests/test_strings.py
@@ -10,3 +10,14 @@ class TestStrings(unittest.TestCase):
         :return: True if the result is expected value, False else
         """
         self.assertEqual(strings.capitalize("python"), "Python")
+
+    def test_multiple_words(self):
+        """
+        Tests capitalize function with multiple words
+        :return:
+        """
+        self.assertEqual(strings.capitalize("python is genial"), "Python is genial")
+
+
+if __name__ == '__main__':
+    unittest.main()
